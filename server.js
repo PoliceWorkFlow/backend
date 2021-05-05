33017e9e195f0d603ba8a1e5f9ad3c1115a7882c
DPSingh0906 <79261005+DPSingh0906@users.noreply.github.com>
2021-05-05 21:58:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ const db = knex({
   client: 'pg',
   connection: {
     host: '127.0.0.1',
-    user: 'postgres',
-    password: '1234',
-    database: 'postgres',
+    user: 'dilpreet',
+    password: 'dilpreet',
+    database: 'police',
   }
 });
 
